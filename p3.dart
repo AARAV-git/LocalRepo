9ca099a0911e9e8398ac5ac3b99ae83cc92ffa85
AARAV-git <sunnypathak979@gmail.com>
2024-10-04 22:46:09 +0530

    Add name

--- p3.dart
+++ p3.dart
@@ -1,8 +1,12 @@
 void main(){
   var Date=printtodaydate();
   print(Date);
+  print(name());
 }
 int printtodaydate(){
   print(DateTime.now());
   return 0;
+}
+String name(){
+  return "AARAV";
 }
